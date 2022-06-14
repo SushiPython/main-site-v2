29c9643db65001b94149ed88d69d91d77b013377
Sushi <github@sushipython.us>
2022-06-14 12:54:48 -0600

    projects kinda

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # flask boilerplate
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -10,5 +11,12 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    # open projects.json and read
+    with open('projects.json', encoding="utf-8") as projects_file:
+        return render_template('projects.html', projects=projects_file)
+    
 
 app.run(host='0.0.0.0', port=8080)
